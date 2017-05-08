efde3fb00429a8000bf70f71bdb0d2b23f7f1e37
akashrajkn <akashrajkn@gmail.com>
2017-05-08 23:56:05 +0530

    Network Analysis - inital commit 1. Added layout 2. Initialized analysis files

--- JASP-Desktop/mainwindow.cpp
+++ JASP-Desktop/mainwindow.cpp
@@ -65,6 +65,7 @@
 
 #ifdef QT_DEBUG
 #include "analysisforms/basregressionlinearlinkform.h"
+#include "analysisforms/Network/networkanalysisform.h"
 #endif
 #include "analysisforms/Network/networkanalysisform.h"
 
@@ -781,6 +782,8 @@ AnalysisForm* MainWindow::loadForm(const string name)
 #ifdef QT_DEBUG
 	else if (name == "BASRegressionLinearLink")
 		form = new BASRegressionLinearLinkForm(contentArea);
+	else if (name == "NetworkAnalysis")
+		form = new NetworkAnalysisForm(contentArea);
 #endif
 	else if (name == "NetworkAnalysis")
 		form = new NetworkAnalysisForm(contentArea);
