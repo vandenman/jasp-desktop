085b510dd9400c92f4fcb2eab54f7b13381f8542
vandenman <donvdbergh@hotmail.com>
2019-08-09 10:24:33 +0200

    updates

--- JASP-Engine/JASP/R/regressionlogisticbayesian.R
+++ JASP-Engine/JASP/R/regressionlogisticbayesian.R
@@ -17,6 +17,15 @@
 # regLogB: regressionLogisticBayesian
 RegressionLogisticBayesian <- function(jaspResults, dataset, options) {
 
+  dataset <- .regLogBayReadData(dataset, options)
+  .regLogBayErrorHandling      (dataset, options)
+ 
+  model <- .regLogBayTableModelComparison(jaspResults, dataset, options)
+  
+  .regLogBayTableSummary(jaspResults, options, model)
+  
+  return()
+  
   # # STATE SYSTEM
   # # load state
   # state <- .retrieveState()
@@ -98,14 +107,7 @@ RegressionLogisticBayesian <- function(jaspResults, dataset, options) {
   #   factorDescriptives <- state[["factorDescriptives"]]
   # }
 
-  dataset <- .regLogBayReadData(dataset, options)
-  .regLogBayErrorHandling      (dataset, options)
- 
-  model <- .regLogBayTableModelComparison(jaspResults, dataset, options)
-  
-  .regLogBayTableSummary(jaspResults, options, model)
-  
-  return()
+
   # RESULTS GENERATION
   # for each non-null result, generate results
   if (is.null(lrObj)) {
@@ -179,10 +181,14 @@ RegressionLogisticBayesian <- function(jaspResults, dataset, options) {
   factorVars <- factorVars[factorVars != ""]
 
   dataset <- .readDataSetToEnd(
-    columns.as.numeric = numericVars,
-    columns.as.factor = factorVars,
+    columns  = c(numericVars, factorVars),
+    # columns.as.numeric = numericVars,
+    # columns.as.factor = factorVars,
     exclude.na.listwise = c(numericVars, factorVars)
   )
+  for (var in .v(factorVars))
+    dataset[[var]] <- factor(dataset[[var]])
+
   return(dataset)
 }
 
@@ -217,10 +223,6 @@ RegressionLogisticBayesian <- function(jaspResults, dataset, options) {
 
 .regLogBayFitBAS <- function(dataset, options) {
 
-  ready <- options[["dependent"]] != "" && !(length(options[["covariates"]]) == 0 || length(options[["factors"]]) == 0L)
-  if (!ready)
-    return(NULL)
-  
   options[["includeIntercept"]] <- TRUE # <- needed for .createGlmFormula
   formula <- .createGlmFormula(options)
   
@@ -231,8 +233,11 @@ RegressionLogisticBayesian <- function(jaspResults, dataset, options) {
   } else {
     wlsWeights <- rep.int(1L, nrow(dataset))
   }
-browser()
-debugonce(BAS::bas.glm)
+  
+  # TODO: BAS produces output for the predictors explain this
+  for (var in .v(options[["factors"]]))
+    levels(dataset[[var]]) <- paste0(" (", factor(dataset[[var]]), ")")
+  
   model <- try(BAS::bas.glm(
     formula     = formula, 
     family      = stats::binomial(link = "logit"),
@@ -242,56 +247,22 @@ debugonce(BAS::bas.glm)
   ))
   if (!isTryError(model)) {
     
+    # rename the variables from ".v(name) (level): .v(name) (level)" to "name (level) * name (level)".
+    interactionSymbol <- "\u2009\u273B\u2009"
     namesx <- model[["namesx"]][-1L]
-    vars  <- all.vars(model[["terms"]])[-1]
-    
-    
-    
-    # regex <- paste0(paste0(vars, collapse = "|"))
-    # ll <- stringr::str_split_fixed(namesx, regex, n = Inf)
-    # unique(gsub(":", "", ll)[ll != ""]))
-    # regex2 <- paste0("^((?!", regex, ").)*$")
-    # 
-    
-    browser()
-    
-    tt <- list(model = model$namesx, terms = 0)
-    attr(tt[["terms"]], "intercept") <- FALSE
-    sapply(model$namesx[-1], .formatTerm, glmModel = tt)
-    
-    model.frame(model$terms, dataset, contrasts)
-    fromto <- attr(model[["terms"]], "term.labels")
-    attr(model[["terms"]], "dataClasses")
-    
-    
-    
-    regex <- paste0(paste0(vars, collapse = "|"))
-    regex2 <- paste0("^((?!", regex, ").)*$")
-                    
-    regex <- paste0("(?<=(", paste0(vars, collapse = "|"), "|\\:))")
-    
-    
-    all <- unlist
-    
-    ll <- stringr::str_split(model$namesx, regex2)
-    
-    
-    lookup <- matrix(NA, length(ll), 2L)
-    for (i in seq_along(ll)) {
-      
+    newNamesx <- character(length(namesx))
+    namesxSplit <- strsplit(namesx, ":")
+    for (i in seq_along(namesxSplit)) {
+      nmSplit <- strsplit(namesxSplit[[i]], " ")
+      for (j in seq_along(nmSplit)) {
+        isVariable <- !startsWith(nmSplit[[j]], "(")
+        nmSplit[[j]][isVariable] <- .unv(nmSplit[[j]][isVariable])
+        nmSplit[[j]] <- paste(nmSplit[[j]], collapse = " ")
+      }
+      namesxSplit[[i]] <- paste(unlist(nmSplit), collapse = interactionSymbol)
     }
-    
-    
-    idx <- pmatch(fromto, model[["namesx"]])
-    pmatch(model[["namesx"]], fromto)
-    startsWith(model[["namesx"]][3], fromto[2])
-    stringr::str_sub(model[["namesx"]][idx], nchar(fromto)+1)
-    
-    
-    formula
-    stringr::str_replace_all(model$namesx[-1L], fromto)
-    
-    model$namesx
+    model[["newNamesx"]] <- c("Intercept", unlist(namesxSplit))
+    model[["BFM"]] <- (model[["postprobs"]] / (1 - model[["postprobs"]])) / (model[["priorprobs"]] / (1 - model[["priorprobs"]]))
     
   }
   
@@ -300,76 +271,54 @@ debugonce(BAS::bas.glm)
 
 .regLogBayTableModelComparison <- function(jaspResults, dataset, options) {
 
-  if (!is.null(jaspResults[["modelComparisonTable"]]))
-    return()
-  
-  modelComparisonTable <- createJaspTable(title = "Model Comparison")
-  jaspResults[["modelComparisonTable"]] <- modelComparisonTable
-  
-  modelComparisonTable$dependOnOptions(c("bayesFactorType", "bayesFactorOrder"))#, "shownModels", "numShownModels"))
-  modelComparisonTable$addCitation(
-    "Clyde, M. A. (2017). BAS: Bayesian Adaptive Sampling for Bayesian Model Averaging. (Version 1.5.3)[Computer software]."
-  )
-  modelComparisonTable$addCitation(
-    "Clyde, M. A., Ghosh, J., & Littman, M. L. (2011). Bayesian adaptive sampling for variable selection and model averaging. 
-    Journal of Computational and Graphical Statistics, 20, 80-101."
-  )
+  if (is.null(jaspResults[["modelContainer"]]))
+    jaspResults[["modelContainer"]] <- createJaspContainer(dependencies = c("dependent", "covariates", "factors", "modelTerms"))
+  else if (!is.null(jaspResults[["modelContainer"]][["modelComparisonTable"]]))
+    return(jaspResults[["modelContainer"]][["modelState"]]$object)
 
-  bf.title <- switch(
-    options[["bayesFactorType"]],
-    "BF10"  = "BF<sub>10</sub>",
-    "BF01"  = "BF<sub>01</sub>",
-    "LogBF10" = "Log(BF<sub>10</sub>)"
-  )
+  modelComparisonTable <- .regLogBayTableModelComparisonMarkup(options)
+  jaspResults[["modelContainer"]][["modelComparisonTable"]] <- modelComparisonTable
 
-  modelComparisonTable$addColumnInfo(name = "Models",         type = "string")
-  modelComparisonTable$addColumnInfo(name = "priorProbModel", type = "number", format = "sf:4;dp:3", title = "P(M)")
-  modelComparisonTable$addColumnInfo(name = "postProbModel",  type = "number", format = "sf:4;dp:3", title = "P(M|data)")
-  modelComparisonTable$addColumnInfo(name = "BFM",            type = "number", format = "sf:4;dp:3", title = "BF<sub>M</sub>")
-  modelComparisonTable$addColumnInfo(name = "BF",             type = "number", format = "sf:4;dp:3", title = bf.title)
-  modelComparisonTable$addColumnInfo(name = "Deviance",       type = "number", format = "dp:3",      title = "Deviance")
-  modelComparisonTable$setExpectedRows(1) 
-  
-  model <- jaspResults[["stateModel"]]$object
-  if (is.null(model))
-    model <- .regLogBayFitBAS(dataset, options)
+  results <- jaspResults[["modelContainer"]][["modelState"]]$object
+  results["ready"] <- .regLogBayIsReady(options)
+  if (results[["ready"]]) { 
+    if (is.null(results[["model"]]))
+      results[["model"]] <- .regLogBayFitBAS(dataset, options)
 
-  if (!isTryError(model)) {
-    table <- .regLogBayFillTableModelComparison(modelComparisonTable, model, options)
-    jaspResults[["stateModel"]] <- createJaspState(model)
-    # jaspResults[["stateTable"]] <- createJaspState(model)
-    return(model)
-  } else {
-    modelComparisonTable$setError(.extractErrorMessage(model))
-    return(NULL)
+    if (isTryError(results[["model"]]))
+      jaspResults[["modelContainer"]]$setError(.extractErrorMessage(results[["model"]]))
+    else
+      .regLogBayFillTableModelComparison(modelComparisonTable, results[["model"]], options)
+
+    jaspResults[["modelContainer"]][["modelState"]] <- createJaspState(results)
   }
+  return(results)
 }
 
-.regLogBayFillTableModelComparison <- function(jaspTable, model, options, stateTable) {
-  
-  allTerms <- .unvf(model[["namesx"]][-1L]) # drop "Intercept"
-  modelsNames <- character(length(model[["which"]]))
-  modelsNames[[1L]] <- "Null model" # TODO: including
-  for (i in 2:length(modelsNames)) {
+.regLogBayFillTableModelComparison <- function(jaspTable, model, options) {
+
+  allTerms <- model[["newNamesx"]]
+  modelNames <- character(length(model[["which"]]))
+  modelNames[1L] <- "Null model" # TODO: including
+  for (i in 2:length(modelNames)) {
     idx <- model[["which"]][[i]][-1]
-    modelsNames[i] <- paste(allTerms[idx], collapse = " + ")
+    modelNames[i] <- paste(allTerms[idx], collapse = " + ")
   }
 
   idx <- if (options[["bayesFactorOrder"]] == "bestModelTop") which.max(model$logmarg) else 1L
   bf <- switch(
     options[["bayesFactorType"]],
-    "BF10"    = exp(model$logmarg - model$logmarg[idx]),
+    "BF10"    = exp(model$logmarg      - model$logmarg[idx]),
     "BF01"    = exp(model$logmarg[idx] - model$logmarg),
-    "LogBF10" = model$logmarg - model$logmarg[idx]
+    "LogBF10" =     model$logmarg      - model$logmarg[idx]
   )
-  BFM <- (model[["postprobs"]] / (1 - model[["postprobs"]])) / (model[["priorprobs"]] / (1 - model[["priorprobs"]]))
 
   table <- data.frame(
-    Models         = modelsNames,
+    Models         = modelNames,
     priorProbModel = model[["priorprobs"]],
     postProbModel  = model[["postprobs"]],
     BF             = bf,
-    BFM            = BFM,
+    BFM            = model[["BFM"]],
     Deviance       = model[["deviance"]]
   )
   table <- table[order(table[, "BF"], decreasing = TRUE), ]
@@ -380,8 +329,49 @@ debugonce(BAS::bas.glm)
 }
 
 .regLogBayTableSummary <- function(jaspResults, options, model) {
+  # browser()
+  # if (!options$summary)
   
-  browser()
+  if (jaspResults[["modelContainer"]]$getError() || ! model[["ready"]])
+    return()
+  
+
   
   
 }
+
+# helper functions ----
+.regLogBayCitations <- c(
+  "Clyde2017"     = "Clyde, M. A. (2017). BAS: Bayesian Adaptive Sampling for Bayesian Model Averaging. (Version 1.5.3)[Computer software].", 
+  "ClydeEtal2011" = "Clyde, M. A., Ghosh, J., & Littman, M. L. (2011). Bayesian adaptive sampling for variable selection and model averaging. Journal of Computational and Graphical Statistics, 20, 80-101."
+)
+
+.regLogBayIsReady <- function(options) {
+  return(list(ready = all(
+    options[["dependent"]] != "",
+    length(options[["covariates"]]) > 0L || length(options[["factors"]]) > 0L,
+    length(options[["modelTerms"]]) > 0L
+  )))
+}
+
+.regLogBayTableModelComparisonMarkup <- function(options) {
+    
+    modelComparisonTable <- createJaspTable(title = "Model Comparison")
+    modelComparisonTable$dependOn(c("bayesFactorType", "bayesFactorOrder"))#, "shownModels", "numShownModels"))
+    modelComparisonTable$addCitation(.regLogBayCitations[c("Clyde2017", "ClydeEtal2011")])
+  
+    bf.title <- switch(
+      options[["bayesFactorType"]],
+      "BF10"  = "BF<sub>10</sub>",
+      "BF01"  = "BF<sub>01</sub>",
+      "LogBF10" = "Log(BF<sub>10</sub>)"
+    )
+  
+    modelComparisonTable$addColumnInfo(name = "Models",         type = "string")
+    modelComparisonTable$addColumnInfo(name = "priorProbModel", type = "number", title = "P(M)")
+    modelComparisonTable$addColumnInfo(name = "postProbModel",  type = "number", title = "P(M|data)")
+    modelComparisonTable$addColumnInfo(name = "BFM",            type = "number", title = "BF<sub>M</sub>")
+    modelComparisonTable$addColumnInfo(name = "BF",             type = "number", title = bf.title)
+    modelComparisonTable$addColumnInfo(name = "Deviance",       type = "number", format = "dp:3", title = "Deviance")
+    return(modelComparisonTable)
+  }
